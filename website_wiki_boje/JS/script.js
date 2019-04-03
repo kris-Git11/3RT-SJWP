01f44098d4e1bdc501ee7436f3f6f225f05b49a1
juricaobadic <jurica.obadic1@gmail.com>
2019-04-03 08:31:06 +0200

    Update

---
+++ website_wiki_boje/JS/script.js
@@ -0,0 +1,45 @@
+let root = document.documentElement;
+
+function setBlue() {
+  root.style.setProperty("--color","aqua");
+};
+
+function setGreen() {
+  root.style.setProperty("--color","limegreen");
+};
+
+function setYellow() {
+  root.style.setProperty("--color","yellow");
+};
+
+function setOrange() {
+  root.style.setProperty("--color","darkorange");
+};
+
+function setRed() {
+  root.style.setProperty("--color","crimson");
+};
+
+function setPink() {
+  root.style.setProperty("--color","hotpink");
+};
+
+var dark = true;
+
+function darkMode() {
+  if(dark == true){
+    dark = false;
+    root.style.setProperty("--background-color","#ddd");
+    root.style.setProperty("--background-color-back","#eee");
+    root.style.setProperty("--background-color-secondary","#eee");
+    root.style.setProperty("--text-color","black");
+    root.style.setProperty("--shadow","rgba(0, 0, 0, 0.2)");
+  }else{
+    dark = true;
+    root.style.setProperty("--background-color","#1A1A1D");
+    root.style.setProperty("--background-color-back","#161618");
+    root.style.setProperty("--background-color-secondary","#1F1F22");
+    root.style.setProperty("--text-color","white");
+    root.style.setProperty("--shadow","rgba(0, 0, 0, 0.5)");
+  }
+};
